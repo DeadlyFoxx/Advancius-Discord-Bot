ba5ba0d72ca9742b12f391b5d672b71b2d20432c
DeadlyFoxx <48535002+DeadlyFoxx@users.noreply.github.com>
2020-05-04 18:11:14 -0400

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,18 @@
+{
+    "red": "D21717",
+    "orange": "FD8506",
+    "fall": "9b5800",
+    "spice": "ebb270",
+    "yellow": "FFEA4A",
+    "green": "06FD28",
+    "forest": "0f5204",
+    "aqua": "00F7FF",
+    "blue": "0C00FF",
+    "cyan": "00b187",
+    "purple": "8506FD",
+    "lightp": "a660e0",
+    "pink": "FD067E",
+    "ocean": "4b6b6b",
+    "white": "FFFFFF",
+    "black": "000000"
+}
